7d71f4ebb19dc810bb8fff3f75b6fa90ab8c9eaa
Peng Li <peng2017@Pengs-MBP.fios-router.home>
2020-07-17 16:29:59 -0400

    exchange service

---
+++ src/main/java/com/training/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,51 @@
+package com.training.microservices.currencyexchangeservice;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private ExchangeValueRepository repo;
+	
+	private Logger logger= LoggerFactory.getLogger(this.getClass());
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		ExchangeValue ev= repo.findByFromAndTo(from, to);
+		ev.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		logger.info("{}", ev);
+		return ev;
+	}
+	
+	@GetMapping("/currency-exchange/allExchangeValues")
+	public List<ExchangeValue> retrieveAllExchangeValues() {
+		
+		return repo.findAll();
+	}
+	
+	@PostMapping("/currency-exchange/addExchangeValue")
+	public void postExchangeValue(@RequestBody ExchangeValue exchangeValue) throws Exception {
+		
+		if (repo.findById(exchangeValue.getId()).isPresent()
+				|| repo.findByFromAndTo(exchangeValue.getFrom(), exchangeValue.getTo()) != null) {
+			//TODO: CIC guys might have an error page or something we can direct this message to.
+			throw new Exception("An exchangeValue with id: " + exchangeValue.getId() 
+				+ " or from: " + exchangeValue.getFrom() + " and to: " + exchangeValue.getTo() + " already exists");
+		}
+		exchangeValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		repo.save(exchangeValue);
+	}
+
+}
